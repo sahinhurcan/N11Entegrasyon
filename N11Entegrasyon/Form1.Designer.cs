1d2637a9a79ffac76ab349600a580ed35668f5c6
Şahin Hürcan <36879997+sahinhurcan@users.noreply.github.com>
2020-10-18 16:59:41 +0300

    Initial commit

---
+++ N11Entegrasyon/Form1.Designer.cs
@@ -0,0 +1,308 @@
+﻿namespace N11Entegrasyon
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnSil = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnUrunGetir = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(111, 124);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(186, 23);
+            this.btnSil.TabIndex = 0;
+            this.btnSil.Text = "Ürün Koduna Göre Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Silinecek Ürün ID:";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(125, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Ürün Listesi:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(125, 57);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // btnUrunGetir
+            // 
+            this.btnUrunGetir.Location = new System.Drawing.Point(231, 56);
+            this.btnUrunGetir.Name = "btnUrunGetir";
+            this.btnUrunGetir.Size = new System.Drawing.Size(97, 23);
+            this.btnUrunGetir.TabIndex = 5;
+            this.btnUrunGetir.Text = "Ürünleri Getir";
+            this.btnUrunGetir.UseVisualStyleBackColor = true;
+            this.btnUrunGetir.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(421, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(334, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "product.productSellerCode(Ürün Mağaza Kodu)";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(421, 165);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "product.title";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(421, 226);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(141, 16);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "product.description\t";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(421, 195);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(114, 16);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "product.subtitle";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(421, 287);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(99, 16);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "product.price";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.Location = new System.Drawing.Point(421, 256);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(142, 16);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "product.category.id";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label9.Location = new System.Drawing.Point(421, 470);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(266, 16);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "product.stockItems.stockItem.quantity";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label10.Location = new System.Drawing.Point(421, 439);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(192, 16);
+            this.label10.TabIndex = 16;
+            this.label10.Text = "product.shipmentTemplate";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label11.Location = new System.Drawing.Point(421, 409);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(181, 16);
+            this.label11.TabIndex = 15;
+            this.label11.Text = "product.productCondition";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label12.Location = new System.Drawing.Point(421, 378);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(203, 16);
+            this.label12.TabIndex = 14;
+            this.label12.Text = "product.images.image.order";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label13.Location = new System.Drawing.Point(421, 348);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(183, 16);
+            this.label13.TabIndex = 13;
+            this.label13.Text = "product.images.image.url";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label14.Location = new System.Drawing.Point(421, 314);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(159, 16);
+            this.label14.TabIndex = 12;
+            this.label14.Text = "product.currencyType";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(732, 195);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(302, 241);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "ÜRÜNÜ EKLE";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_2);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(111, 153);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(186, 23);
+            this.button2.TabIndex = 19;
+            this.button2.Text = "Mağaza Koduna Göre Sil";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1182, 630);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnUrunGetir);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSil);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "N11 Entegrasyon";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnUrunGetir;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ N11Entegrasyon/Form1.cs
@@ -0,0 +1,172 @@
+﻿using N11Entegrasyon.UrunServis;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using N11Entegrasyon;
+
+namespace N11Entegrasyon
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+                        
+
+        }
+        //public void ApiBilgisi()
+        //{
+        //    Authentication auth = new Authentication();
+        //    auth.appKey = "7094ef60-aa6d-4eed-ba67-9c7c0665eceb";
+        //    auth.appSecret = "QBKFaVBCC2jCLC1f";
+        //}
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            //DeleteProductByIdRequest urunsil = new DeleteProductByIdRequest();
+            //Authentication auth = new Authentication();
+            //auth.appKey = "7094ef60-aa6d-4eed-ba67-9c7c0665eceb";
+            //auth.appSecret = "QBKFaVBCC2jCLC1f";
+            //urunsil.auth = auth;
+            //urunsil.productId = 436061583;
+
+            var auth = new Authentication();
+            auth.appKey = "7094ef60-aa6d-4eed-ba67-9c7c0665eceb"; //api anahtarınız
+            auth.appSecret = "QBKFaVBCC2jCLC1f"; ;//api şifeniz
+
+            ProductServicePortService silPort = new ProductServicePortService();
+            DeleteProductByIdRequest request = new DeleteProductByIdRequest();
+            request.auth = auth;
+            request.productId = 436273013;
+
+
+            DeleteProductByIdResponse response = new DeleteProductByIdResponse();
+            var result = silPort.DeleteProductById(request);
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            //var auth = new Authentication();
+            //auth.appKey = "7094ef60-aa6d-4eed-ba67-9c7c0665eceb"; //api anahtarınız
+            //auth.appSecret = "QBKFaVBCC2jCLC1f"; ;//api şifeniz
+
+            //ProductServicePortService proxy = new ProductServicePortService();
+            
+            //request.auth = auth;
+            //request.productId = 436061583;
+
+
+            //GetProductByProductIdResponse response = new GetProductByProductIdResponse();
+            //var result = proxy.DeleteProductById(request);
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_2(object sender, EventArgs e)
+        {
+            
+            var auth = new Authentication();
+            auth.appKey = "API-KEY"; //api anahtarınız
+            auth.appSecret = "API-SIFRE"; ;//api şifeniz
+
+            ProductServicePortService prodServ = new ProductServicePortService();
+
+            SaveProductRequest saveRequest = new SaveProductRequest();
+            //request.auth = auth;
+            //request.productId = "436061583";
+            saveRequest.product.productSellerCode = "5555";
+            saveRequest.product.title = "TEST YAZILIM ÜRÜNÜ";
+            saveRequest.product.subtitle = "1 Ay Ürünümüzü Test Edin";
+            saveRequest.product.description = " 1 aylık demo kurulum lisansıdır.mağazamızdan sınırsız lisans da alabilirsiniz";
+            saveRequest.product.category.id = 1000320;
+            //saveRequest.product.price =  4.99;
+            saveRequest.product.currencyType = "TL";
+            //ProductImage UrunGorsel = new ProductImage();
+            //UrunGorsel.url = "https://n11scdn.akamaized.net/a1/1024/elektronik/ofis-programlari/teknik-serv-is-programi__0216739852896260.jpg";
+            //saveRequest.product.images.image.url = UrunGorsel;
+            //saveRequest.product.images.image.order = "0";
+            saveRequest.product.productCondition = "1";
+            saveRequest.product.preparingDay = "2";
+            saveRequest.product.shipmentTemplate = "ucretsiz";
+
+
+            //request.product.stockItems.stockItem.quantity = 3;
+
+            //var result = proxy.SaveProductRequest(request);
+            SaveProductResponse saveResponse = prodServ.SaveProduct(saveRequest);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var auth = new Authentication();
+            auth.appKey = "API-KEY"; //api anahtarınız
+            auth.appSecret = "API-SIFRE"; ;//api şifeniz
+
+            ProductServicePortService silPort = new ProductServicePortService();
+            DeleteProductBySellerCodeRequest UrunSil = new DeleteProductBySellerCodeRequest();
+            UrunSil.auth = auth;
+            UrunSil.productSellerCode = "5555";
+
+            DeleteProductBySellerCodeResponse response = new DeleteProductBySellerCodeResponse();
+            var result = silPort.DeleteProductBySellerCode(UrunSil);
+
+            //-----------------------------------------------------------//
+            /*
+            var auth = new Authentication();
+            auth.appKey = "7094ef60-aa6d-4eed-ba67-9c7c0665eceb"; //api anahtarınız
+            auth.appSecret = "QBKFaVBCC2jCLC1f"; ;//api şifeniz
+
+            ProductServicePortService silPort = new ProductServicePortService();
+            DeleteProductByIdRequest request = new DeleteProductByIdRequest();
+            request.auth = auth;
+            request.productId = 436273013;
+
+
+            DeleteProductByIdResponse response = new DeleteProductByIdResponse();
+            var result = silPort.DeleteProductById(request);
+            */
+
+
+        }
+    }
+}
